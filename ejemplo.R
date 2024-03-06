47f96bd750cfdf4f731a633a5af6af085a7f0217
MariferP <mpena23@alumnos.uaq.mx>
2024-03-06 10:20:19 -0600

    allsl

---
+++ ejemplo.R
@@ -0,0 +1,18 @@
+#ejercicio1mariferpena
+#ksdmasjkdn
+
+########################################
+##TAREA 1- MARIA FERNANDA PEÑA LEDESMA##
+########################################
+
+#############################
+#PARTE 1/  Preguntas rápidas#
+#############################
+
+#Instalación de librerías
+install.packages("igraph")
+library(igraph)
+
+#1.
+g15 <- make_empty_graph(n=15, directed = TRUE)
+plot(g15)
